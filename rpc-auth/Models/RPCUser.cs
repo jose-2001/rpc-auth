a0870bb482a771cdc181ad7cb87e1375246206bc
JoseA <josealejandro.garcia@hotmail.com>
2021-09-05 14:05:37 -0500

    changed how some column titles are displayed, changed url creation format. Before deleting password column from index

--- rpc-auth/Models/RPCUser.cs
+++ rpc-auth/Models/RPCUser.cs
@@ -11,7 +11,9 @@ namespace rpc_auth.Models
         public int ID { get; set; }
         public string username { get; set; }
         public string password { get; set; }
+        [Display(Name = "First Name")] 
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")] 
         public string LastName{ get; set; }
         [DataType(DataType.Date)]
         public DateTime Birthdate { get; set; }
