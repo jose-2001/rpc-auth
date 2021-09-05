fa00395a784afa8e113ae765f2cb7d19cafbfb63
JoseA <josealejandro.garcia@hotmail.com>
2021-09-05 12:07:37 -0500

    scaffolded items added

---
+++ rpc-auth/Pages/Users/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using rpc_auth.Data;
+using rpc_auth.Models;
+
+namespace rpc_auth.Pages.Users
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly rpc_auth.Data.rpc_authContext _context;
+
+        public DetailsModel(rpc_auth.Data.rpc_authContext context)
+        {
+            _context = context;
+        }
+
+        public User User { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            User = await _context.User.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (User == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ rpc-auth/Pages/Users/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using rpc_auth.Data;
+using rpc_auth.Models;
+
+namespace rpc_auth.Pages.Users
+{
+    public class IndexModel : PageModel
+    {
+        private readonly rpc_auth.Data.rpc_authContext _context;
+
+        public IndexModel(rpc_auth.Data.rpc_authContext context)
+        {
+            _context = context;
+        }
+
+        public IList<User> User { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            User = await _context.User.ToListAsync();
+        }
+    }
+}
